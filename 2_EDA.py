9c3c241e0216bc32069e61030dc47336621df209
Mei Chen Liu <meichenliu83@gmail.com>
2024-12-08 13:27:24 +0800

    update

--- 2_EDA.py
+++ 2_EDA.py
@@ -19,3 +19,31 @@ print(music_tracks_df.describe())
 # podcast episodes
 print(podcast_episodes_df.info())
 print(podcast_episodes_df.describe())
+
+
+# check for null values in music data
+print(music_tracks_df.isnull().sum())
+
+# check for null values in podcast data
+print(podcast_episodes_df.isnull().sum())
+
+'''
+The null values are associated with false in offline column.
+Thus the null values are valid.
+'''
+
+# EDA with visuals
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import rcParams
+
+# set font to Microsoft YaHei to show Chinese characters
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+
+# visualize top 20 most played artists
+top_artists = music_tracks_df['artist_name'].value_counts().head(20)
+sns.barplot(x=top_artists.values, y = top_artists.index, palette='Blues_r')
+plt.title('Top 20 Most Played Artists')
+plt.xlabel('Artist')
+plt.ylabel('Number of Plays')
+plt.show()
---
+++ test.py
@@ -0,0 +1,2 @@
+from matplotlib import font_manager
+print([f.name for f in font_manager.fontManager.ttflist])
