932fbd6b3bacd57055135688b7284d7999fde064
Mei Chen Liu <meichenliu83@gmail.com>
2024-12-09 16:03:16 +0800

    update

--- 2_Exploratory_Data_Analysis/README.md
+++ 2_Exploratory_Data_Analysis/README.md
@@ -69,7 +69,29 @@ Below are the questions I want to answer in my project:
   Thus the null values are valid.
   '''
   ``` 
-- **Visualize Outliners:**
+- **Identify Outliners:**
+  ``` python
+  music_tracks_df = pd.read_csv('./Cleaned_Data/Music_Streaming_History.csv')
+  podcast_episodes_df = pd.read_csv('./Cleaned_Data/Podcast_Streaming_History.csv')
+  # create boxplot for minutes_played in music streaming dataset
+  plt.figure(figsize=(10,8))
+  # use minutes_played to create the boxplot
+  sns.boxplot(x=music_tracks_df['minutes_played'], palette='Blues_r')
+  plt.title('Boxplot of Minutes Played (Music)', fontsize = 20)
+  plt.xlabel('Minutes Played')
+  plt.show()
+
+  # create boxplot for minutes_played in postcast streaming dataset
+  plt.figure(figsize=(10,8))
+  # use minutes played to create the boxplot
+  sns.boxplot(x=podcast_episodes_df['minutes_played'], palette = 'Blues_r')
+  plt.title('Boxplot of Minuets Played (Podcast)', fontsize = 20)
+  plt.xlabel('Minuetes Played')
+  plt.show()
+  ```
+  ![music_boxplot](/Images/music_boxplot.png)
+  ![podcast_boxplot](/Images/podcast_boxplot.png)
+  
 
 - **Visulaize Distributions:**  
 
--- test.py
+++ test.py
@@ -6,25 +6,19 @@ from matplotlib import rcParams
 music_tracks_df = pd.read_csv('./Cleaned_Data/Music_Streaming_History.csv')
 podcast_episodes_df = pd.read_csv('./Cleaned_Data/Podcast_Streaming_History.csv')
 
-# set font to Microsoft YaHei to show Chinese characters
-plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
-# visualize top 20 most played artists
-top_artists = music_tracks_df['artist_name'].value_counts().head(20)
-sns.barplot(x=top_artists.values, y = top_artists.index, palette='Blues_r')
-plt.title('Top 20 Most Played Artists', fontsize = 20)
-plt.xlabel('Number of Plays')
-plt.ylabel(None)
+# create boxplot for minutes_played in music streaming dataset
+plt.figure(figsize=(10,8))
+# use minutes_played to create the boxplot
+sns.boxplot(x=music_tracks_df['minutes_played'], palette='Blues_r')
+plt.title('Boxplot of Minutes Played (Music)', fontsize = 20)
+plt.xlabel('Minutes Played')
 plt.show()
 
-# visualize top 20 most played shows (podcast)
-top_show = podcast_episodes_df['show_name'].value_counts().head(20)
-# adjust width and height
-plt.figure(figsize=(10, 8))  
-sns.barplot(x=top_show.values, y = top_show.index, palette='Blues_r')
-plt.title('Top 20 Most Played Shows', fontsize = 20)
-plt.xlabel('Number of Plays')
-plt.ylabel(None)
-# ensure everything fits properly
-plt.tight_layout()
+# create boxplot for minutes_played in postcast streaming dataset
+plt.figure(figsize=(10,8))
+# use minutes played to create the boxplot
+sns.boxplot(x=podcast_episodes_df['minutes_played'], palette = 'Blues_r')
+plt.title('Boxplot of Minuets Played (Podcast)', fontsize = 20)
+plt.xlabel('Minuetes Played')
 plt.show()
